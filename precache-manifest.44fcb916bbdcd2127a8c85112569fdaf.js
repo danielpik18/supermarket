f93628e3b9969f3140186474c831e0a40e84324a
Daniel Mantilla <danielmomusic@gmail.com>
2019-05-06 09:09:19 -0500

    Updates

---
+++ precache-manifest.44fcb916bbdcd2127a8c85112569fdaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1270d2664323b09712c5395e75a2f73d",
+    "url": "/danielpik18/supermarket/index.html"
+  },
+  {
+    "revision": "d55ed8ffbfcdc18c1ca9",
+    "url": "/danielpik18/supermarket/static/css/main.74431d95.chunk.css"
+  },
+  {
+    "revision": "1495e5cd80ec8d5b8068",
+    "url": "/danielpik18/supermarket/static/js/2.15b93246.chunk.js"
+  },
+  {
+    "revision": "d55ed8ffbfcdc18c1ca9",
+    "url": "/danielpik18/supermarket/static/js/main.5d6c9472.chunk.js"
+  },
+  {
+    "revision": "43c6a1a0cbee81a2790b",
+    "url": "/danielpik18/supermarket/static/js/runtime~main.6d812dfa.js"
+  }
+]);
